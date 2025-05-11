2d68ce37738f7366849f6f0de6a7ed195fb7b84a
zakinori <chogifu@gmail.com>
2025-05-11 19:08:57 +0900

    fix: 職歴や資格を確認できるようにした

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
+import { Experience } from "./components/Experience";
 import { Footer } from "./components/Footer";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
         <Hero />
         <Projects />
         <Skills />
+        <Experience />
       </main>
       <Footer />
     </div>
